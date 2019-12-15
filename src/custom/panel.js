c8be166a9325b89b8df5109cfb1f91d5f4f0d8e5
Mohanad-Badawy <43563000+Mohanad-Badawy@users.noreply.github.com>
2019-12-15 19:16:19 +0400

    test1
    1

---
+++ src/custom/panel.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CustTable from "./table";
+
+function CustPanel() {
+  return (
+    <div>
+      <h2>Start edit ing to see some magic happen!</h2>
+      <CustTable name="dear" />
+    </div>
+  );
+}
+
+export default CustPanel;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CustPanel from "./custom/panel.js";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Header!</h1>
+      <CustPanel />
     </div>
   );
 }
