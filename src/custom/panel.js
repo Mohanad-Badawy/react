ed2482fb30ebadca2d9a923cd933a5fd6abec908
Mohanad-Badawy <43563000+Mohanad-Badawy@users.noreply.github.com>
2019-12-22 17:27:24 +0400

    state 2

--- src/custom/panel.js
+++ src/custom/panel.js
@@ -1,37 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import CustTable from "./table";
 import axios from "axios";
 
 function CustPanel() {
-  const [dataTbl, setDataTbl] = useState([]);
+  const [dataTbl, setDataTbl] = useState(null);
 
   async function fetchUrl() {
     await axios
       .get("https://www.mocky.io/v2/5dfb3ef62f00006200ff9d62")
+      //  .then(response => console.log(response.data))
       .then(
         response => {
+          console.log("log --{}", response.data);
           setDataTbl(response.data);
-          console.log("log --" + dataTbl);
+          console.log("dataTbl Loaded ...");
         },
         error => {
-          console.log("--> " + error);
+          console.log("--> ", error);
         }
       )
+      .catch(error => {
+        console.log("Error fetching and parsing data", error);
+      })
       .finally(() => {
-        console.log("finally dataTbl" + dataTbl);
+        console.log("finally dataTbl ", dataTbl);
       });
   }
 
+  // init call only one time
   useEffect(() => {
     fetchUrl();
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div>
-      <h2>Start edit ing to see some magic happen!</h2>
-      <CustTable name="name" />
+      <h2>Start edit ing to see some magic happen! {dataTbl == null ? 'loading..':dataTbl.responseCode} </h2>
+      <CustTable name={dataTbl == null ? 'loading..':dataTbl.responseCode} />
+
     </div>
   );
 }
--- src/custom/table.js
+++ src/custom/table.js
@@ -2,23 +2,19 @@ import React from "react";
 import Table from "@material-ui/core/Table";
 import TableContainer from "@material-ui/core/TableContainer";
 import Paper from "@material-ui/core/Paper";
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 function CustTable(props) {
   return (
     <div>
       <h1>Hello, {props.name}</h1>;
       <TableContainer component={Paper}>
-        <Table >
-        <TableHead>
-          <TableRow>
-
-
-
-          </TableRow>
+        <Table>
+          <TableHead>
+            <TableRow />
           </TableHead>
         </Table>
       </TableContainer>
