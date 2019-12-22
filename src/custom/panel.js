c6f4236ed7378c6e513800997cae605eaed73bb0
Mohanad-Badawy <43563000+Mohanad-Badawy@users.noreply.github.com>
2019-12-22 11:32:38 +0400

    stage 2

--- src/custom/panel.js
+++ src/custom/panel.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CustTable from "./table";
+import axios from "axios";
 
 function CustPanel() {
+  const [dataTbl, setDataTbl] = useState([]);
+
+  async function fetchUrl() {
+    await axios
+      .get("https://www.mocky.io/v2/5dfb3ef62f00006200ff9d62")
+      .then(
+        response => {
+          setDataTbl(response.data);
+          console.log("log --" + dataTbl);
+        },
+        error => {
+          console.log("--> " + error);
+        }
+      )
+      .finally(() => {
+        console.log("finally dataTbl" + dataTbl);
+      });
+  }
+
+  useEffect(() => {
+    fetchUrl();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <h2>Start edit ing to see some magic happen!</h2>
-      <CustTable name="dear" />
+      <CustTable name="name" />
     </div>
   );
 }
--- src/custom/table.js
+++ src/custom/table.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Table from "@material-ui/core/Table";
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from "@material-ui/core/Paper";
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 function CustTable(props) {
   return (
     <div>
       <h1>Hello, {props.name}</h1>;
-      <Table  />
+      <TableContainer component={Paper}>
+        <Table >
+        <TableHead>
+          <TableRow>
+
+
+
+          </TableRow>
+          </TableHead>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
